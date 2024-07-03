d50d7a37a706ad61c3ecf27cfd8fedc298fc7a15
Austin-Choi <austinsupra@gmail.com>
2024-07-03 21:16:24 +0900

    feature: TodoList 디자인 및 CSS 적용

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,10 +1,11 @@
 import React from "react";
 import TodoItem from "./TodoItem";
+import "./TodoInput.css";
 
 const TodoList = ({ todos, onDelete, onToggleComplete }) => {
   return (
     <div>
-      <h3>할일 목록</h3>
+      <h2 className="todo-text">할일 목록</h2>
       {todos.map((todo, index) => (
         <TodoItem
           key={index}
